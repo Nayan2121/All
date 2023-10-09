1c0ffaeef7b3bdec0344e74435922aa052dcb09c
Nayan2121 <bambhaniyanayan21@gmail.com>
2023-10-09 17:00:52 +0530

    9-10-23

--- React-2/Main Project/eTrade/src/App.jsx
+++ React-2/Main Project/eTrade/src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 import './App.css'
-// import { Home } from './Pages/home.jsx'
+import { Home } from './Pages/home.jsx'
 
 
 function App() {
@@ -8,6 +8,7 @@ function App() {
 
   return (
     <>
+    <Home/>
     </>
   )
 }
--- React-2/Main Project/eTrade/src/Pages/Home.jsx
+++ React-2/Main Project/eTrade/src/Pages/Home.jsx
@@ -1,11 +1,27 @@
 import React from "react";
-import { Navbar } from "./Nabar";
+import { NavBar } from "../components/Navbar/NavBar";
+import { DropdownHover1, DropdownHover2, Rightpart } from "../components/Dropdown/DropdownHover"
 
-export default function Home() {
+export function Home() {
   return (
-    <div className="m-0 px-24 bg-red-400 h-screen">
-      <Navbar/>
-    </div>
+    <>
+      <div className="h-screen bg-[#F9F3F0] px-64">
+
+        <section className='py-2'>
+          <div className='flex justify-between'>
+            <div className='flex'>
+              <DropdownHover1 />
+              <DropdownHover2 />
+            </div>
+            <Rightpart />
+          </div>
+        </section>
+
+        <section>
+          <NavBar />
+        </section>
+
+      </div>
+    </>
   )
 }
-
